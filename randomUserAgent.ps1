5f3ac7fcae5ea833b147230234c56094eee7bc86
ToishY <31921460+ToishY@users.noreply.github.com>
2019-05-02 00:02:13 +0200

    Add files via upload

---
+++ randomUserAgent.ps1
@@ -0,0 +1,39 @@
+﻿[CmdletBinding()]
+<# 
+    .SYNOPSIS 
+        Retrieve a random user-agent from text file.
+
+    .PARAMETER file 
+        The literal file path of the text file containing user-agents.
+        Default [int] 20.
+
+    .EXAMPLE  
+        .\randomUserAgent.ps1
+
+    .EXAMPLE  
+        .\randomString.ps1 -file "C:\Users\Kintoki\user_agents.txt"
+
+    .NOTES 
+        NAME:       randomUserAgent
+        AUTHOR:     ToishY
+        GIT:        https://github.com/ToishY/randomUserAgent
+        CREATED:    05/01/2019 
+        LASTEDIT:   05/01/2019
+        VERSION:    1.0
+#>
+
+Param(
+    [Parameter(Mandatory=$false)]
+    [string]$file = 'user_agents.txt'
+)
+
+function getRandomUserAgent{
+    Param(
+        [Parameter(Mandatory=$true)]
+        [string]$ua_file
+    )
+    return ( Get-Content -LiteralPath $ua_file | Sort{Get-Random} | Select -First 1 )
+}
+
+# Call randomString
+getRandomUserAgent -ua_file $file
